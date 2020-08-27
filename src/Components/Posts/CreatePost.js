ccab0f0280a1d28b24a10852c8e1206c7f073cd3
prachijustin <prachi.sharma01sep@gmail.com>
2020-08-27 09:00:02 +0530

    create post form

--- src/Components/Posts/CreatePost.js
+++ src/Components/Posts/CreatePost.js
@@ -11,23 +11,29 @@ const CreatePost = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <h2>POsts</h2>
-      <h3>Add Post</h3>
-      <input
-        type="text"
-        value={post.title}
-        className="form-control"
-        onChange={(e) => setPost({ ...post, title: e.target.value })}
-      />
-      <textarea
-        type="text"
-        value={post.description}
-        className="form-control"
-        onChange={(e) => setPost({ ...post, description: e.target.value })}
-      />
-      <button className="btn btn-success">Submit</button>
-    </form>
+    <div className="container">
+      <form onSubmit={handleSubmit}>
+        <h2>POsts</h2>
+        <h3>Add Post</h3>
+        <input
+          type="text"
+          value={post.title}
+          className="form-control"
+          placeholder="title"
+          onChange={(e) => setPost({ ...post, title: e.target.value })}
+        />
+        <br />
+        <textarea
+          type="text"
+          value={post.description}
+          className="form-control"
+          placeholder="description"
+          onChange={(e) => setPost({ ...post, description: e.target.value })}
+        />
+        <br />
+        <button className="btn btn-success">Submit</button>
+      </form>
+    </div>
   );
 };
 
